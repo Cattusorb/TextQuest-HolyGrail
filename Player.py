a8115d21b69cf7910d54ac85a8047a5a5d03e114
Roslyn Parker <magicmachineplaymc@gmail.com>
2019-11-20 10:24:53 -0500

    Updated Player.py
    working for characters

--- Player.py
+++ Player.py
@@ -3,53 +3,71 @@
 # End Date:
 
 class Player:
-	def __init__(self, name):
-    '''
-        Constructor for class sets specific 
-        characteristics for a given character
-    '''
-    if name == "King Arthur":
-	self.name = name
-	self.strength = 8
-	self.health = 6
-	self.wits = 8
-    elif name == "Sir Lancelot":
-	self.name = name
-	self.strength = 10
-	self.health = 7
-	self.wits = 5
-    elif name == "Sir Bedivere": 
-	self.name = name
-	self.strength = 5
-	self.health = 7
-	self.wits = 6
-    elif name == "Sir Robin":
-	self.name = name
-	self.strength = 4
-	self.health = 5
-	self.wits = 8
-    elif name == "Sir Galahad":
-	self.name = name
-	self.strength = 8
-	self.health = 5
-	self.wits = 4
-    elif name == "Sir Not Appearing in this Game": 
-        self.name = "End"
-    else: 
-        self.name = "End"
-      
+
+    def __init__(self, name):
+        '''
+            Constructor for Player class sets
+            specific characteristics for each 
+            given character
+        '''
+        
+        if name == "King Arthur":
+            self.name = name
+            self.strength = 8
+            self.health = 6
+            self.wits = 8
+        elif name == "Sir Lancelot":
+            self.name = name
+            self.strength = 10
+            self.health = 7
+            self.wits = 5
+        elif name == "Sir Bedivere": 
+            self.name = name
+            self.strength = 5
+            self.health = 7
+            self.wits = 6
+        elif name == "Sir Robin":
+            self.name = name
+            self.strength = 4
+            self.health = 5
+            self.wits = 8
+        elif name == "Sir Galahad":
+            self.name = name
+            self.strength = 8
+            self.health = 5
+            self.wits = 4
+        elif name == "Sir Not Appearing in this Game": 
+            self.name = "End"
+        else: 
+            self.name = "End"
+            
+    def display_player(self):
+        '''
+            Displays the player with character stats 
+            to the user, nicely formatted
+        '''
+        if self.name == "End":
+            print("\tPlayer not registered.\n")
+            return
+            
+        print(f"\t{self.name}")
+        print("\t~-...*...-~")
+        print(f"\tStrength {self.strength}")
+        print(f"\tHealth {self.health}")
+        print(f"\tWits {self.wits}\n")
+     
     def get_strength(self): 
         '''
             Returns strength of character
         '''
-        return self.strength
+        return self.strength  
     
     def get_health(self):
         '''
             Returns health of character
         '''
-        return self.health
-        
+        return self.health 
+      
     def get_wits(self):
         '''
             Returns wits of character 
@@ -65,31 +83,31 @@ class Player:
         '''
             Sets strength of character to given value
         '''
-	if value < 0:
-		self.strength = 0
-	elif value > 10: 
-		self.strength = 10
+        if value < 0:
+            self.strength = 0
+        elif value > 10: 
+            self.strength = 10
         else: 
-		self.strength = value
-    
+            self.strength = value
+
     def set_health(self, value):
         '''
             Sets health of character to given value
         '''
         if value < 0:
-		self.health = 0
-	elif value > 10: 
-		self.health = 10
+            self.health = 0
+        elif value > 10: 
+            self.health = 10
         else: 
-		self.health = value
-        
+            self.health = value
+            
     def set_wits(self, value):
         '''
             Sets wits of character to given value
         '''
         if value < 0:
-		self.wits = 0
-	elif value > 10: 
-		self.wits = 10
+            self.wits = 0
+        elif value > 10: 
+            self.wits = 10
         else: 
-		self.wits = value
+            self.wits = value
